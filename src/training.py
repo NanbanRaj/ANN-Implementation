5d3b89e035d1c88225ead5b22cb84e905b57811c
NanbanRaj <rajasimman67@gmail.com>
2021-10-20 09:27:44 +0530

    Added a save plot

--- src/training.py
+++ src/training.py
@@ -2,6 +2,7 @@ from utils.common import read_config
 from utils.data_mgmt import get_data
 from utils.model import create_model
 from utils.model import save_model
+from utils.save_polt import save_plot
 
 import os
 
@@ -35,6 +36,11 @@ def training(config_path):
     os.makedirs(model_dir_path,exist_ok=True)
 
     save_model(model, model_name, model_dir_path)
+
+    plot_dir = config['artifacts']['plots_dir']
+    plot_name = config['artifacts']['plot_name']
+
+    save_plot(histroy, plot_name, plot_dir)
 
 if __name__ == '__main__':
     # args = argparse.ArgumentParser()
